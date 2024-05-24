9b201fdd504291c1fa7d6c0dc513b847fac33f11
Kyrylo <kyrylocolti4@gmail.com>
2024-05-24 22:22:31 +0300

    feat: get a file

--- app/main.js
+++ app/main.js
@@ -1,11 +1,22 @@
 const net = require("net");
-const EventEmitter = require("node:events");
+const fs = require("fs");
+const path = require("path");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
+function getResponse(headers, body) {
+  let response = "HTTP/1.1 200 OK\r\n";
+  for (const [key, value] of Object.entries(headers)) {
+    response += `${key}: ${value}\r\n`;
+  }
+  response += "\r\n";
+  response += body;
+  return response;
+}
+
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+  socket.on("data", async (data) => {
     const request = data.toString();
     const splitedRequest = request.split("\r\n");
     const requestTarget = splitedRequest[0].split(" ")[1];
@@ -23,15 +34,34 @@ const server = net.createServer((socket) => {
     } else if (requestTarget.startsWith("/user-agent")) {
       const userAgent = splitedRequest
         .find((value) => value.startsWith("User-Agent"))
-        .split(": ")[1];
+        ?.split(": ")[1];
 
       const responseHeaders = {
         "Content-Type": "text/plain",
-        "Content-Length": userAgent.length,
+        "Content-Length": userAgent?.length,
       };
 
       const response = `HTTP/1.1 200 OK\r\n${Object.entries(responseHeaders).join("\r\n").replace(/,/g, ": ")}\r\n\r\n${userAgent}`;
       socket.write(response);
+    } else if (requestTarget.startsWith("/files/")) {
+      const requestFilePath = requestTarget.split("/")[2];
+      const directory = process.argv[3];
+
+      const resolvedFilePath = path.resolve(directory, requestFilePath);
+
+      fs.stat(resolvedFilePath, async (err, stats) => {
+        if (err) {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+          return;
+        }
+        const responseHeaders = {
+          "Content-Type": "application/octet-stream",
+        };
+        fs.readFile(resolvedFilePath, (err, buffer) => {
+          const response = getResponse(responseHeaders, buffer);
+          socket.write(response);
+        });
+      });
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
